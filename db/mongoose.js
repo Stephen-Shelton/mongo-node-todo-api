bc03c460e3185a05330db6558d23e84e3a85f639
stephen-shelton <stephenshelton777@gmail.com>
2016-12-10 14:55:24 -0800

    deleted extra db folder

--- db/mongoose.js
+++
@@ -1,8 +0,0 @@
-var mongoose = require('mongoose');
-
-mongoose.Promise = global.Promise; //tells mongoose we're using native promises
-mongoose.connect('mongodb://localhost:27017/TodoApp');
-
-module.exports = {
-  mongoose
-};
